84efbb969185a7a17d8c460780c5838d511273d5
Kaio Delphino <dash.zet@gmail.com>
2025-04-05 14:00:00 -0300

    feat: implement transaction split

---
+++ lib/src/entities/transaction_split.dart
@@ -0,0 +1,6 @@
+class TransactionSplit {
+  TransactionSplit(this.userId, this.percentage) : assert(percentage > 0), assert(percentage <= 1);
+
+  final String userId;
+  final double percentage;
+}
