c620bf9589fdcfce2087bca6de0f7e02c852825a
Kaio Delphino <dash.zet@gmail.com>
2025-04-13 21:19:48 -0300

    wip

---
+++ lib/src/entities/transaction.dart
@@ -0,0 +1,28 @@
+import "budget.dart";
+import "tag.dart";
+import "transaction_split.dart";
+
+class Transaction {
+  Transaction({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.budget,
+    required this.value,
+    required this.createdAt,
+    required this.tags,
+    required this.splits,
+  }) : assert(
+         splits.map((e) => e.percentage).reduce((a, b) => a + b) == 1,
+         "splits percentage must sum up to 1 (100% of a transaction value)",
+       );
+
+  final String id;
+  final String name;
+  final String description;
+  final Budget budget;
+  final int value;
+  final DateTime createdAt;
+  final List<Tag> tags;
+  final List<TransactionSplit> splits;
+}
