4c3fd83f415360a489a71057d32e70d495db2ee0
Kaio Delphino <dash.zet@gmail.com>
2025-09-13 18:17:50 -0300

    feat(transaction): add constructor for transactions not saved in db yet

--- lib/src/transaction/transaction.dart
+++ lib/src/transaction/transaction.dart
@@ -1,4 +1,3 @@
-import "package:cobudget/src/entities/budget.dart";
 import "package:cobudget/src/entities/tag.dart";
 import "package:cobudget/src/entities/transaction_split.dart";
 import "package:cobudget/src/utils/aliases.dart";
@@ -15,13 +14,42 @@ class Transaction {
     required this.splits,
     this.externalId,
   }) : assert(
-         splits.map((e) => e.percentage).reduce((a, b) => a + b) == 1,
+         splits.isEmpty || splits.map((e) => e.percentage).reduce((a, b) => a + b) == 1,
          "splits percentage must sum up to 1 (100% of a transaction value)",
        ),
        assert(description == null || description.isNotEmpty),
-       assert(localId > 0);
+       assert(
+         localId != 0,
+         "should be a negative integer (not saved) or positive (saved in database)",
+       );
 
-  final int localId;
+  // Factory method for creating new unsaved transactions
+  factory Transaction.unsaved({
+    required String name,
+    required double value,
+    required List<Tag> tags,
+    required List<TransactionSplit> splits,
+    LocalId? budget,
+    String? description,
+    DateTime? createdAt,
+  }) {
+    final DateTime creation = createdAt ?? DateTime.now();
+    final int temporaryId = -creation.millisecondsSinceEpoch;
+    assert(temporaryId < 0);
+
+    return Transaction(
+      localId: temporaryId,
+      name: name,
+      description: description,
+      budget: budget,
+      value: value,
+      createdAt: creation,
+      tags: tags,
+      splits: splits,
+    );
+  }
+
+  LocalId localId;
   final String? externalId;
   final String name;
   final String? description;
@@ -30,6 +58,9 @@ class Transaction {
   final DateTime createdAt;
   final List<Tag> tags;
   final List<TransactionSplit> splits;
+
+  bool get isSaved => localId > 0;
+  bool get isUnsaved => !isSaved;
 
   bool get isSynced => externalId != null;
   bool get isUnsynced => !isSynced;
